423a773097e2e24bc6e823533aa32832c0d6e216
Andrew <parksungho320@gmail.com>
2022-01-04 06:02:50 +0000

    Added css, fish.js and apis folder

--- acnh_helper/src/components/App.js
+++ acnh_helper/src/components/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import '../css/App.css';
 const App = () => {
 	return (
-		<div>HELLO WORLD</div>
+		<div className="main">HELLO WORLD</div>
 	);
 }
 
