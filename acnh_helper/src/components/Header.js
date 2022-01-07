4814c506dc8fbaca11ffef1784ee152951d6b235
Andrew <parksungho320@gmail.com>
2022-01-07 01:07:30 +0000

    Added header and router wire up

--- acnh_helper/src/components/Header.js
+++ acnh_helper/src/components/Header.js
@@ -4,14 +4,36 @@ import {Link} from 'react-router-dom';
 
 const Header = () => {
 	return (
-		<div>
-			<nav className="navbar navbar-light bg-light">
-				<Link to="/catch/fish" className="navbar-brand">
-					<img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" />
-					BootStrap
-				</Link>
-			</nav>
-		</div>
+		<nav className="navbar navbar-expand-lg navbar-light" style={{background: "#e3f2fd"}}>
+  			<div className="container-fluid">
+    			<a className="navbar-brand" href="#">Navbar</a>
+    			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      				<span className="navbar-toggler-icon"></span>
+    			</button>
+    		<div className="collapse navbar-collapse" id="navbarNav">
+      			<ul className="navbar-nav mr-auto">
+        			<li className="nav-item">
+						<Link className="nav-link active" aria-current="page" to="/">Home</Link>
+					</li>
+        			<li className="nav-item">
+          				<Link to="/catch/fish" className="nav-link">
+		 					<img src="http://acnhapi.com/v1/icons/fish/1" width="30" height="30" className="d-inline-block align-top" alt="" />
+		 					Fish
+		 				</Link>
+        			</li>
+        			<li className="nav-item">
+          				<Link to="/collect/fossils" className="nav-link">
+							<img src="http://acnhapi.com/v1/images/fossils/plesio_skull" width="30" height="30" className="d-inline-block align-top" alt="" />
+							Fossils
+						</Link>
+        			</li>
+      			</ul>
+				<ul className="nav me-auto me-1">
+					login button
+				</ul>
+    		</div>
+  			</div>
+		</nav>
 	);
 }
 				
